2cdad096424716798745897c9b765c7e419147cc
SomaticTom <129551581+SomaticTom@users.noreply.github.com>
2025-05-23 21:36:02 -0700

    * Implemented firmware backup feature with date and time stamp * Implemented WiFi credentials setting * General cleanup * Fixed browse dialog to see both *.hex and *.bin files

--- FlashUtility.py
+++ FlashUtility.py
@@ -7,6 +7,9 @@ import sys
 import threading
 import serial.tools.list_ports
 import re
+from datetime import datetime
+import base64
+
 
 class TextRedirector(object):
     def __init__(self, widget, rootwin):
@@ -14,25 +17,24 @@ class TextRedirector(object):
         self.rootwin = rootwin
 
     def write(self, string):
-        value = re.findall("\(([0-9]*) \%\)", string)
-        #print(value)
-        # self.widget["value"] = value
+        value = re.findall(r"\(([0-9]*) \%\)", string)
         if value:
-            # self.widget.insert("end", int(value[0]))
             self.widget["value"] = int(value[0])
-        # self.widget.configure(state="disabled")
         self.rootwin.update_idletasks()
 
     def flush(self):
         pass
 
+
 class SomaticErosFirmwareUpdater:
     def __init__(self, root):
         self.root = root
         self.root.geometry("640x480")
-        self.root.title("Somatic Eros Firmware Update")
+        self.root.title("Somatic Eros Firmware Updater")
 
-        self.label = tk.Label(root, text="Please plug in your tracker and hold down the button while flashing.")
+        self.firmware_file = "firmware.hex"  # Specify the local firmware file name
+
+        self.label = tk.Label(root, text="Please plug in your tracker. If the flashing fails, try holding down the button while flashing.")
         self.label.pack(pady=10)
 
         # self.file_path_var = tk.StringVar()
@@ -50,31 +52,52 @@ class SomaticErosFirmwareUpdater:
         # self.text_output = tk.Text(root, height=5, width=40)#, yscrollcommand=self.v_scrollbar.set)
         # self.text_output.pack(side=tk.BOTTOM)
 
-        # self.download_progress = ttk.Progressbar(root, orient="horizontal", length=300, mode="determinate")
-        self.update_progress = ttk.Progressbar(root, orient="horizontal", length=300, maximum = 100, mode="determinate")
-        self.update_progress.pack(side=tk.BOTTOM, fill=tk.X)
-        sys.stdout = TextRedirector(self.update_progress, root)
+        self.update_progressbar = ttk.Progressbar(root, orient="horizontal", length=300, maximum=100, mode="determinate")
+        self.update_progressbar.pack(side=tk.BOTTOM, fill=tk.X)
+        sys.stdout = TextRedirector(self.update_progressbar, root)
 
-        self.update_button = tk.Button(root, text="Update Firmware", command=self.open_port_window, state=tk.DISABLED)
+        self.backup_label = tk.Label(root, text="We suggest backing up your firmware for at least one tracker before updating!")
+        self.backup_label.pack(pady=10)
+
+        self.backup_button = tk.Button(root, text="Backup Firmware", command=lambda: self.open_port_window(followUpFunction=self.backup_firmware))
+        self.backup_button.pack(pady=10)
+
+        self.update_button = tk.Button(root, text="Update Firmware", command=lambda: self.open_port_window(followUpFunction=self.update_firmware), state=tk.DISABLED)
         self.update_button.pack(pady=10)
 
-        # Download firmware file from the new URL
+        self.wifiSSID_label = tk.Label(root, text="WiFi SSID:")
+        self.wifiSSID_entry = tk.Entry(root)
+        self.wifiSSID_label.pack(pady=10)
+        self.wifiSSID_entry.pack(padx=10)
+
+        self.wifiPass_label = tk.Label(root, text="WiFi Password:")
+        self.wifiPass_entry = tk.Entry(root, show="*")
+        self.wifiPass_label.pack(pady=10)
+        self.wifiPass_entry.pack(padx=10)
+
+        self.wifi_button = ttk.Button(root, text="Send WiFi Credentials", command= lambda: self.open_port_window(followUpFunction=self.send_wifi_creds))
+        self.wifi_button.pack(pady=10)
+
+        # Download firmware file from the repository
         firmware_url = "https://github.com/SomaticVR/SomaticVR-Eros-Flasher/raw/main/Firmware/firmware.hex"
-        self.firmware_file = "firmware.hex"  # Specify the local file name
 
         download_thread = threading.Thread(target=self.download_firmware, args=(firmware_url, self.firmware_file))
         download_thread.start()
 
     def browse_file(self):
-        file_path = filedialog.askopenfilename(filetypes=[("Firmware Files", "*.bin, *.hex")])
+        file_path = filedialog.askopenfilename(filetypes=[("Firmware Files", "*.bin *.hex")])
         if file_path is not None:
-            self.firmware_file = file_path
+            if (file_path != ""):
+                self.firmware_file = file_path
+                self.update_status(f"{file_path} selected")
+            else:
+                self.update_status("No file selected")
 
-    def open_port_window(self):
+    def open_port_window(self, followUpFunction):
         # Get a list of serial ports that match the specified criteria
         port_list = [port.device for port in serial.tools.list_ports.comports() if "303A:1001" in port.hwid]
         if (len(port_list) == 1):
-            self.update_firmware(port_list[0])
+            followUpFunction(port_list[0])
         elif (len(port_list) > 1):
             port_window = tk.Toplevel(self.root)
             port_window.title("Select Serial Port")
@@ -82,42 +105,18 @@ class SomaticErosFirmwareUpdater:
             port_label = tk.Label(port_window, text="Select Serial Port:")
             port_label.pack(pady=10)
 
-
             port_var = tk.StringVar(value=port_list)
 
             port_menu = tk.OptionMenu(port_window, port_var, *port_list)
             port_menu.pack(pady=10)
 
-            confirm_button = tk.Button(port_window, text="Confirm", command= self.update_firmware(port_var.get(), port_window))#lambda port_var, port_window: threading.Thread(target = self.update_firmware, args = (port_var.get(), port_window)).start())
+            confirm_button = tk.Button(port_window, text="Confirm", command=lambda: followUpFunction(port_var.get(), port_window))  # lambda port_var, port_window: threading.Thread(target = self.update_firmware, args = (port_var.get(), port_window)).start())
             confirm_button.pack(pady=10)
         else:
             pass
 
-    def download_firmware(self, firmware_url, firmware_file):
-        try:
-            response = requests.get(firmware_url, stream=True)
-            total_size = int(response.headers.get('content-length', 0))
-
-            # self.download_progress.pack(pady=10)
-            self.root.update_idletasks()
-
-            with open(firmware_file, 'wb') as file:
-                for data in response.iter_content(chunk_size=1024):
-                    file.write(data)
-                    # self.download_progress["value"] += len(data)
-                    self.root.update_idletasks()
-
-            self.update_status("Firmware downloaded successfully!")
-            self.update_progress["value"] = 0  # Reset progress bar for the next update
-
-            # Enable the "Update Firmware" button
-            self.update_button["state"] = tk.NORMAL
-
-        except requests.exceptions.RequestException as e:
-            self.update_status(f"Failed to download firmware: {e}")
-
-    def update_firmware(self, selected_port, port_window = None):
-        self.update_progress["value"] = 0  # Reset progress bar for the next update
+    def update_firmware(self, selected_port, port_window=None):
+        self.update_progressbar["value"] = 0  # Reset progress bar for the next update
         if not self.firmware_file:
             self.update_status("Please select a firmware file.")
             return
@@ -136,7 +135,7 @@ class SomaticErosFirmwareUpdater:
         self.update_status("Firmware update started... Please wait")
         try:
             # Run esptool to flash the firmware
-            command = ['--chip', 'esp32c3', '--port', selected_port, '--baud', '460800'] 
+            command = ['--chip', 'esp32c3', '--port', selected_port, '--baud', '460800']
             command.extend(['--before', 'default_reset', '--after', 'hard_reset', 'write_flash', '-z', '--flash_mode', 'dio'])
             command.extend(['--flash_freq', '80m', '--flash_size', '4MB'])
             # command.extend(['0x0000', './Firmware/bootloader.bin'])
@@ -144,7 +143,6 @@ class SomaticErosFirmwareUpdater:
             # command.extend(['0xe000', './Firmware/boot_app0.bin'])
             # command.extend(['0x10000', firmware_file])
             command.extend(['0x0000', self.firmware_file])
-
 
             print(f'Using command {" ".join(command)}')
             esptool.main(command)
@@ -156,14 +154,99 @@ class SomaticErosFirmwareUpdater:
 
         self.root.update_idletasks()
 
-    def set_update_progress(self, position, total):
+    def backup_firmware(self, selected_port, port_window=None):
+        self.update_progressbar["value"] = 0  # Reset progress bar for the next update
+        # If no port is selected, use the first port in the list
+        if not selected_port:
+            port_list = [port.device for port in serial.tools.list_ports.comports() if "303A:1001" in port.hwid]
+            if port_list:
+                selected_port = port_list[0]
+            else:
+                self.update_status("No suitable serial ports found.")
+                return
+
+        if port_window is not None:
+            port_window.destroy()  # Close the port selection window
+        firmwareBackupFilename = os.path.join(os.getcwd(), f'firmwareBackup-{datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.bin')
+        self.update_status(f"Backing up firmware to {firmwareBackupFilename}... Please wait")
+        try:
+            # Run esptool to flash the firmware
+            command = ['--chip', 'esp32c3', '--port', selected_port, '--baud', '460800']
+            command.extend(['read_flash', '0x00000', '0x150000', firmwareBackupFilename])
+            command.extend(['--flash_size', '4MB'])
+
+            esptool.main(command)
+
+            self.update_status("Firmware saved successfully!")
+
+        except Exception as e:
+            self.update_status(f"Firmware backup failed: {e}")
+
+        self.root.update_idletasks()
+
+    def send_wifi_creds(self, selected_port, port_window=None):
+        # If no port is selected, use the first port in the list
+        if not selected_port:
+            port_list = [port.device for port in serial.tools.list_ports.comports() if "303A:1001" in port.hwid]
+            if port_list:
+                selected_port = port_list[0]
+            else:
+                self.update_status("No suitable serial ports found.")
+                return
+
+        if port_window is not None:
+            port_window.destroy()  # Close the port selection window
+
+        with serial.Serial(selected_port, 115200, timeout=1) as ser:
+            self.update_status(f"connecting to {ser.name}")
+            command = f"SET BWIFI {base64.b64encode(self.wifiSSID_entry.get().encode('utf-8')).decode('ascii')} {base64.b64encode(self.wifiPass_entry.get().encode('utf-8')).decode('ascii')}\n"
+            # self.update_status(command)
+            try: 
+                if (ser.write(command.encode('ascii')) > 0):
+                    if (ser.read_until(expected=b'CMD SET BWIFI OK:')[-17:] == b'CMD SET BWIFI OK:'):
+                        self.update_status("WiFi information updated successfully!")
+                    else:
+                        self.update_status("WiFi information update failed")
+                else:
+                    self.update_status("WiFi information update failed")
+            except Exception as e:
+                self.update_status(f"WiFi information update failed: {e}")
+
+        self.root.update_idletasks()
+        
+    def download_firmware(self, firmware_url, firmware_file):
+        try:
+            response = requests.get(firmware_url, stream=True)
+            total_size = int(response.headers.get('content-length', 0))
+
+            # self.download_progress.pack(pady=10)
+            self.root.update_idletasks()
+
+            with open(firmware_file, 'wb') as file:
+                for data in response.iter_content(chunk_size=1024):
+                    file.write(data)
+                    # self.download_progress["value"] += len(data)
+                    self.root.update_idletasks()
+
+            self.update_status("Firmware downloaded successfully!")
+            self.update_progressbar["value"] = 0  # Reset progress bar for the next update
+
+            # Enable the "Update Firmware" button
+            self.update_button["state"] = tk.NORMAL
+
+        except requests.exceptions.RequestException as e:
+            self.update_status(f"Failed to download firmware: {e}")
+
+    def update_progress(self, position, total):
         # For esptool.write_flash, position is the number of bytes written, and total is the total size
-        self.update_progress["value"] = position
-        self.update_progress["maximum"] = total
+        self.update_progressbar["value"] = position
+        self.update_progressbar["maximum"] = total
         self.root.update_idletasks()
 
     def update_status(self, message):
         self.status_bar["text"] = message
+        self.root.update_idletasks()
+
 
 if __name__ == "__main__":
     root = tk.Tk()
