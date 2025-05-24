db894a4f9e9e15b307666c02b7f24275cae8805a
SomaticTom <129551581+SomaticTom@users.noreply.github.com>
2025-05-23 21:35:55 -0700

    Initial code commit

---
+++ wait_key.py
@@ -0,0 +1,27 @@
+import termios
+import fcntl
+import sys
+import os
+
+def wait_key():
+    fd = sys.stdin.fileno()
+
+    oldterm = termios.tcgetattr(fd)
+    newattr = termios.tcgetattr(fd)
+    newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+    termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+    oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+    fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+
+    try:
+        while 1:
+            try:
+                c = sys.stdin.read(1)
+                if c:
+                    print(f"Got character {repr(c)}")
+            except IOError: pass
+    finally:
+        termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+        fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
+    return(c)
---
+++ wificred.py
@@ -0,0 +1,22 @@
+import serial.tools.list_ports as port_list
+import serial
+
+if (len(sys.argv) > 2):
+    ssid = str(sys.argv[1])
+    passwd = str(sys.argv[2])
+
+    ports = list(port_list.grep("303A:1001"))
+    print(ports[0].device)
+    if ports:
+        with serial.Serial(ports[0].device, 9600, timeout=1) as ser:
+            # ser.baudrate = 9600
+            # ser.port = ports[0]
+            # ser.open()
+            print (f"connecting to {ser.name}")
+            if (ser.write(f'SET WIFI "{ssid}"" "{passwd}"'.encode('utf-8')) > 0):
+                # print(ser.readline())
+                # if (ser.write(b'REBOOT') > 0):
+                print("WiFi information updated")
+else:
+    print("Error: 2 arguments required")
+    print(f"{sys.argv[0]} [ssid] [passwd]")
