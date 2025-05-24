8e48272f32f0dbff03f72d86f89ee12f5b76aec3
SomaticTom <129551581+SomaticTom@users.noreply.github.com>
2025-05-23 21:36:01 -0700

    Changed firmware file back to .hex format and cleaned up wificred

--- wificred.py
+++ wificred.py
@@ -5,16 +5,11 @@ import sys
 if (len(sys.argv) > 2):
     ssid = str(sys.argv[1])
     passwd = str(sys.argv[2])
-
     ports = list(port_list.grep("303A:1001"))
-    print(ports[0].device)
     if ports:
         with serial.Serial(ports[0].device, 9600, timeout=1) as ser:
-            # ser.baudrate = 9600
-            # ser.port = ports[0]
-            # ser.open()
             print (f"connecting to {ser.name}")
-            if (ser.write(f'SET WIFI "{ssid}"" "{passwd}"'.encode('utf-8')) > 0):
+            if (ser.write(f'SET WIFI "{ssid}" "{passwd}"'.encode('utf-8')) > 0):
                 ser.read_until(expected="CMD SET WIFI OK:")
                 print("WiFi information updated")
 else:
